9d667a3c5e7afb798d54d1e151ace9ccac551141
laura20010905 <laura20010905@gmail.com>
2025-10-17 02:02:02 +0000

    correccion

---
+++ lib/inicio.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import './perfil.dart';
+
+class Inicio extends StatelessWidget {
+  const Inicio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: PerfilScreen());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,13 @@
-
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:myapp/firebase_options.dart';
-import 'package:myapp/registro.dart';
-<<<<<<< HEAD
 import 'package:myapp/perfil.dart'; // Importa la nueva pantalla
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/providers/room_provider.dart';
 import 'package:myapp/screens/room_details_screen.dart';
 import 'package:provider/provider.dart';
 
-=======
-import 'package:myapp/perfil.dart';
->>>>>>> 91ea3e9 (Cambios)
+import 'package:myapp/inicio.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return ChangeNotifierProvider(
-      create: (_) => RoomProvider(),
-      child: MaterialApp(
-        title: 'Hotel App',
-        theme: ThemeData(
-          primarySwatch: Colors.teal,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          textTheme: GoogleFonts.robotoTextTheme(Theme.of(context).textTheme),
-=======
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
-        useMaterial3: true,
-      ),
-      home: const LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RegistroPage()),
-                );
-              },
-              child: const Text('Ir a Registro'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const PerfilScreen()),
-                );
-              },
-              child: const Text('Ir a Perfil'),
-            ),
-          ],
->>>>>>> 91ea3e9 (Cambios)
-        ),
-        home: const RoomDetailsScreen(),
-      ),
-    );
-  }
+  runApp(const Inicio());
 }
