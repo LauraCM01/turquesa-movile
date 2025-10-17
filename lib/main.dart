2c74403e29f70ff84f43defc691410737997bc62
laura20010905 <laura20010905@gmail.com>
2025-10-17 23:19:46 +0000

    Cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:myapp/perfil.dart'; // Importa la nueva pantalla
-import 'package:google_fonts/google_fonts.dart';
-import 'package:myapp/providers/room_provider.dart';
-import 'package:myapp/screens/room_details_screen.dart';
-import 'package:provider/provider.dart';
-
 import 'package:myapp/inicio.dart';
 
 void main() async {
